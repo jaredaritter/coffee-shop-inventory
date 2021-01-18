99fb27ddc66d31ff59f490034eef4351c58a08f8
Jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-18 14:34:01 -0500

    Set up mongoose and mongoDB connection.

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Connect MongoDB
+const mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akjwg.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
