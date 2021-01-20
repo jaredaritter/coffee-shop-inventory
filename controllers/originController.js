78ca34cfc5bf7a3a066ce24a91b026d01a2665ea
jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-20 16:30:07 -0500

    Add base origin controller functions.

---
+++ controllers/originController.js
@@ -0,0 +1,38 @@
+// REQUIRED MODULES
+
+// MODEL MODULES
+
+// INDIVIDUAL CONTROLLER FUNCTIONS
+exports.origin_list = function (req, res, next) {
+  res.send('Origin List still needs to be created.');
+};
+
+exports.origin_detail = function (req, res, next) {
+  res.send('Origin Detail still needs to be created.');
+};
+
+exports.origin_create_get = function (req, res, next) {
+  res.send('Origin Create GET still needs to be created.');
+};
+
+exports.origin_create_post = (req, res, next) => {
+  res.send('Origin Create POST still needs to be created.');
+};
+
+exports.origin_update_get = (req, res, next) => {
+  res.send('Origin Update GET still needs to be created.');
+};
+
+exports.origin_update_post = (req, res, next) => {
+  res.send('Origin Update POST still needs to be created.');
+};
+
+exports.origin_delete_get = (req, res, next) => {
+  res.send('Origin Delete GET still needs to be created.');
+};
+
+exports.origin_delete_post = (req, res, next) => {
+  res.send('Origin Delete POST still needs to be created.');
+};
+
+// EXPORTS.NAME ASSIGNMENT OR A MODULE.EXPORTS ASSIGNMENT AT END BOTH WORK TO EXPORT CONTROLLER FUNCTIONS
