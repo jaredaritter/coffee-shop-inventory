81975c2428103e7ea9f055e9cd304cc63b80aec1
jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-20 16:30:18 -0500

    Add base roast controller functions.

---
+++ controllers/roastController.js
@@ -0,0 +1,38 @@
+// REQUIRED MODULES
+
+// MODEL MODULES
+
+// INDIVIDUAL CONTROLLER FUNCTIONS
+exports.roast_list = function (req, res, next) {
+  res.send('Roast List still needs to be created.');
+};
+
+exports.roast_detail = function (req, res, next) {
+  res.send('Roast Detail still needs to be created.');
+};
+
+exports.roast_create_get = function (req, res, next) {
+  res.send('Roast Create GET still needs to be created.');
+};
+
+exports.roast_create_post = (req, res, next) => {
+  res.send('Roast Create POST still needs to be created.');
+};
+
+exports.roast_update_get = (req, res, next) => {
+  res.send('Roast Update GET still needs to be created.');
+};
+
+exports.roast_update_post = (req, res, next) => {
+  res.send('Roast Update POST still needs to be created.');
+};
+
+exports.roast_delete_get = (req, res, next) => {
+  res.send('Roast Delete GET still needs to be created.');
+};
+
+exports.roast_delete_post = (req, res, next) => {
+  res.send('Roast Delete POST still needs to be created.');
+};
+
+// EXPORTS.NAME ASSIGNMENT OR A MODULE.EXPORTS ASSIGNMENT AT END BOTH WORK TO EXPORT CONTROLLER FUNCTIONS
