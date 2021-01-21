45686ad8d421a29afa5130ea435dfa45e6527ffb
jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-21 10:33:13 -0500

    Add roast routes. Router finalized.

--- routes/inventory.js
+++ routes/inventory.js
@@ -11,34 +11,67 @@ const roast_controller = require('../controllers/roastController');
 router.get('/', coffee_controller.index);
 
 /// COFFEE ROUTES ///
-// CREATE
+// CREATE NEW COFFEE
 router.get('/coffee/create', coffee_controller.coffee_create_get);
+
 router.post('/coffee/create', coffee_controller.coffee_create_post);
-// UPDATE
+
+// UPDATE EXISTING COFFEE
 router.get('/coffee/:id/update', coffee_controller.coffee_update_get);
-router.get('/coffee/:id/update', coffee_controller.coffee_update_post);
-// DELETE
+
+router.post('/coffee/:id/update', coffee_controller.coffee_update_post);
+
+// DELETE EXISTING COFFEE
 router.get('/coffee/:id/delete', coffee_controller.coffee_delete_get);
-router.get('/coffee/:id/delete', coffee_controller.coffee_delete_post);
+
+router.post('/coffee/:id/delete', coffee_controller.coffee_delete_post);
+
 // READ (GOES AT END TO PREVENT OVERRIDING OTHER ROUTES)
 router.get('/coffee/:id', coffee_controller.coffee_detail);
+
 router.get('/coffees', coffee_controller.coffee_list);
 
 /// ORIGIN ROUTES ///
-// CREATE
+// CREATE NEW ORIGIN
 router.get('/origin/create', origin_controller.origin_create_get);
+
 router.post('/origin/create', origin_controller.origin_create_post);
-// UPDATE
+
+// UPDATE EXISTING ORIGIN
 router.get('/origin/:id/update', origin_controller.origin_update_get);
+
 router.post('/origin/:id/update', origin_controller.origin_update_post);
-// DELETE
+
+// DELETE EXISTING ORIGIN
 router.get('/origin/:id/delete', origin_controller.origin_delete_get);
-router.post('/origin/:id/delete', origin_controller.origin_update_post);
+
+router.post('/origin/:id/delete', origin_controller.origin_delete_post);
+
 // READ
 router.get('/origin/:id', origin_controller.origin_detail);
+
 router.get('/origins', origin_controller.origin_list);
 
 /// ROAST ROUTES ///
+// CREATE NEW ROAST
+router.get('/roast/create', roast_controller.roast_create_get);
+
+router.post('/roast/create', roast_controller.roast_create_post);
+
+// UPDATE EXISTING ROAST
+router.get('/roast/:id/update', roast_controller.roast_update_get);
+
+router.post('roast/:id/update', roast_controller.roast_update_post);
+
+// DELETE EXISTING ROAST
+router.get('/roast/:id/delete', roast_controller.roast_delete_get);
+
+router.post('/roast/:id/delete', roast_controller.roast_delete_post);
+
+// READ (AT END TO PREVENT OVERRIDING OTHER ROUTES)
+router.get('/roast/:id', roast_controller.roast_detail);
+
+router.get('/roasts', roast_controller.roast_list);
 
 // FINAL EXPORT
 module.exports = router;
