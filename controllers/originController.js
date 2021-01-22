1836b3535f12adc3179116857e599796ece4d9ce
jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-22 08:16:17 -0500

    Add origins and origin detail controllers and views.

--- controllers/originController.js
+++ controllers/originController.js
@@ -1,14 +1,37 @@
 // REQUIRED MODULES
 
 // MODEL MODULES
+const Origin = require('../models/origin');
 
 // INDIVIDUAL CONTROLLER FUNCTIONS
 exports.origin_list = function (req, res, next) {
-  res.send('Origin List still needs to be created.');
+  Origin.find({}, function (err, origin_list) {
+    if (err) {
+      return next(err);
+    }
+    res.render('origin_list', {
+      title: 'Origin List',
+      origin_list: origin_list,
+    });
+  });
 };
 
 exports.origin_detail = function (req, res, next) {
-  res.send('Origin Detail still needs to be created.');
+  Origin.findById(req.params.id).exec(function (err, origin) {
+    if (err) {
+      return next(err);
+    }
+    if (origin === null) {
+      const err = new Error('Origin not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('origin_detail', {
+      title: `Origin: ${origin.country}`,
+      origin: origin,
+      error: err,
+    });
+  });
 };
 
 exports.origin_create_get = function (req, res, next) {
