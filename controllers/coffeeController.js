3b73a26aa648e7d04b1f57c11634c3382d9429d5
jared <38332939+jaredaritter@users.noreply.github.com>
2021-02-03 15:17:14 -0500

    Began implementing coffee create post controller. Not finished.

--- controllers/coffeeController.js
+++ controllers/coffeeController.js
@@ -97,9 +97,89 @@ exports.coffee_create_get = function (req, res, next) {
   );
 };
 
-exports.coffee_create_post = (req, res, next) => {
-  res.send('Coffee Create POST still needs to be created.');
-};
+exports.coffee_create_post = [
+  body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Description must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('origin', 'Origin must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('roast', 'Roast must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('price', 'Price must be number <= 1000')
+    .trim()
+    .isNumeric()
+    .withMessage('Price must be a number.')
+    .isLength({ min: 1, max: 4 })
+    .escape(),
+  body('quantity')
+    .trim()
+    .isNumeric()
+    .withMessage('Quantuty must be a number.')
+    .isLength({ min: 1, max: 3 })
+    .withMessage('Must be 3 digits or less')
+    .escape(),
+  (req, res, next) => {
+    // BUILD ERRORS LIST BASED ON VALIDATION RESULT
+    const errors = validationResult(req);
+
+    // BUILD NEW BOOK
+    const coffee = new Coffee({
+      name: req.body.name,
+      description: req.body.description,
+      origin: req.body.origin,
+      roast: req.body.roast,
+      price: req.body.price,
+      quantity: req.body.quantity,
+    });
+    // ORIGIN AND ROAST NOT SUBMITTING VALUES AND ARE SHOWING UP AS UNDEFINED.
+    console.log(coffee.origin);
+    console.log(coffee.roast);
+    console.log(coffee.price);
+
+    // IF ERRORS IS NOT EMPTY THEN RERENDER PAGE WITH SANITIZED DATA
+    if (!errors.isEmpty()) {
+      // GRAB ORIGIN AND ROAST LISTS AGAIN
+      async.parallel(
+        {
+          origin_list: function (callback) {
+            Origin.find(callback);
+          },
+          roast_list: function (callback) {
+            Roast.find(callback);
+          },
+        },
+        function (err, results) {
+          if (err) {
+            return next(err);
+          }
+          // console.log('Coffee: ' + coffee.origin._id);
+          res.render('coffee_form', {
+            title: 'Create Coffee',
+            coffee: coffee,
+            errors: errors.array(),
+            origin_list: results.origin_list,
+            roast_list: results.roast_list,
+          });
+        }
+      );
+    }
+    // ELSE SAVE GOOD DATA TO DB
+    else {
+      coffee.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(coffee.url);
+      });
+    }
+  },
+];
 
 exports.coffee_update_get = (req, res, next) => {
   res.send('Coffee Update GET still needs to be created.');
